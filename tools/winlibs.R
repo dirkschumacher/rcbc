9a0b566209c6ce2d122dfe1e91a2b70ef41f5e61
Jeffrey Hanson <jeffrey.hanson@uqconnect.edu.au>
2022-12-30 08:58:29 +1300

    fix rwinlib url

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -2,7 +2,7 @@ VERSION <- commandArgs(TRUE)
 if (!file.exists(sprintf("../windows/cbc-%s/include", VERSION))) {
   if (getRversion() < "3.3.0") setInternet2()
   download.file(
-    sprintf("https://github.com/dirkschumacher/cbc/archive/v%s.zip", VERSION),
+    sprintf("https://github.com/rwinlib/cbc/archive/v%s.zip", VERSION),
     "lib.zip", quiet = TRUE
   )
   dir.create("../windows", showWarnings = FALSE)
